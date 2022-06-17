09a3cd33945483fd22c2b312c4e4c9cbd90d2b5c
Landon <landonmarshall1997@gmail.com>
2022-06-16 22:50:51 -0700

    changed to 2d array for board

--- src/logic.ts
+++ src/logic.ts
@@ -1,5 +1,8 @@
 import { InfoResponse, GameState, MoveResponse, Game, Coord } from "./types"
 
+// Avoid Hazards, and snakes for now
+const AVOID_LETTERS = ["H", "S"];
+
 export function info(): InfoResponse {
 	console.log("INFO")
 	const response: InfoResponse = {
@@ -20,7 +23,104 @@ export function end(gameState: GameState): void {
 	console.log(`${gameState.game.id} END\n`)
 }
 
+export function printBoard(board: string[][]): void {
+	const rows = board.length
+	const cols = board[0].length
+
+	// Transpose grid so we can actually see the board
+	let grid: string[][] = []
+	for (let col = 0; col < cols; col++) {
+		grid[col] = []
+	}
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < cols; col++) {
+			grid[cols - col - 1][row] = board[row][col]
+		}
+	}
+	console.table(grid);
+}
+
+export function isDeathLeft(board: string[][], myHead: Coord, ruleset: string): boolean {
+	const boardWidth = board[0].length;
+	const leftBad = AVOID_LETTERS.includes(board?.[myHead.x - 1]?.[myHead.y]);
+	const wrappedLeftBad = ruleset === 'wrapped'
+		&& myHead.x === 0
+		&& AVOID_LETTERS.includes(board?.[boardWidth - 1]?.[myHead.y]);
+	const atLeftWall = myHead.x === 0 && ruleset !== 'wrapped';
+	return leftBad || wrappedLeftBad || atLeftWall;
+}
+
+export function isDeathRight(board: string[][], myHead: Coord, ruleset: string): boolean {
+	const boardWidth = board[0].length;
+	const rightBad = AVOID_LETTERS.includes(board?.[myHead.x + 1]?.[myHead.y]);
+	const wrappedRightBad = ruleset === 'wrapped'
+		&& myHead.x === boardWidth - 1
+		&& AVOID_LETTERS.includes(board?.[0]?.[myHead.y]);
+	const atRightWall = myHead.x === boardWidth - 1 && ruleset !== 'wrapped';
+	return rightBad || wrappedRightBad || atRightWall;
+}
+
+export function isDeathUp(board: string[][], myHead: Coord, ruleset: string): boolean {
+	const boardHeight = board.length;
+	const upBad = AVOID_LETTERS.includes(board?.[myHead.x]?.[myHead.y + 1]);
+	const wrappedUpBad = ruleset === 'wrapped'
+		&& myHead.y === boardHeight - 1
+		&& AVOID_LETTERS.includes(board?.[myHead.x]?.[0]);
+	const atTopWall = myHead.y === boardHeight - 1 && ruleset !== 'wrapped';
+	return upBad || wrappedUpBad || atTopWall;
+}
+
+export function isDeathDown(board: string[][], myHead: Coord, ruleset: string): boolean {
+	const boardHeight = board.length;
+	const downBad = AVOID_LETTERS.includes(board?.[myHead.x]?.[myHead.y - 1]);
+	const wrappedDownBad = ruleset === 'wrapped'
+		&& myHead.y === 0
+		&& AVOID_LETTERS.includes(board?.[myHead.x]?.[boardHeight - 1]);
+	const atBottomWall = myHead.y === 0 && ruleset !== 'wrapped';
+	return downBad || wrappedDownBad || atBottomWall;
+}
+
+export function populateBoard(gameState: GameState, board: string[][]): string[][] {
+	let boardCopy = board;
+	// Add snakes
+	gameState.board.snakes.forEach(snake => {
+		snake.body.forEach(bodyPiece => {
+			boardCopy[bodyPiece.x][bodyPiece.y] = 'S';
+		});
+	});
+
+	// Add hazards
+	if (gameState.board.hazards) {
+		gameState.board.hazards.forEach(hazard => {
+			boardCopy[hazard.x][hazard.y] = 'H';
+		});
+	}
+	// Add food
+	gameState.board.food.forEach(food => {
+		boardCopy[food.x][food.y] = 'F';
+	});
+
+	// Add my head
+	boardCopy[gameState.you.head.x][gameState.you.head.y] = 'O';
+	return board;
+}
+
+export function initializeBoard(gameState: GameState): string[][] {
+	let board: string[][] = [];
+	for (let i = 0; i < gameState.board.width; i++) {
+		board[i] = [];
+		for (let j = 0; j < gameState.board.height; j++) {
+			board[i][j] = '';
+		}
+	}
+	let filledBoard = populateBoard(gameState, board);
+	return filledBoard;
+}
+
+
+
 export function move(gameState: GameState): MoveResponse {
+	let gameBoard: string[][] = initializeBoard(gameState);
 	let possibleMoves: { [key: string]: number } = {
 		up: 0,
 		down: 0,
@@ -33,67 +133,19 @@ export function move(gameState: GameState): MoveResponse {
 
 	// Avoid self collisions
 	const myHead = gameState.you.head
-	const myNeck = gameState.you.body[1]
-
-	if (myNeck.x < myHead.x || (ruleset === "wrapped" && myNeck.x === 0 && (myHead.x === boardWidth - 1))) {
+	// const myNeck = gameState.you.body[1]
+	// printBoard(gameBoard);
+	if (isDeathLeft(gameBoard, myHead, ruleset)) {
 		possibleMoves.left = -99999999999999;
-	} else if (myNeck.x > myHead.x || (ruleset === "wrapped" && (myNeck.x === boardWidth - 1) && myHead.x === 0)) {
+	}
+	if (isDeathRight(gameBoard, myHead, ruleset)) {
 		possibleMoves.right = -99999999999999;
-	} else if (myNeck.y < myHead.y || (ruleset === "wrapped" && myNeck.y === 0 && (myHead.y === boardHeight - 1))) {
-		possibleMoves.down = -99999999999999;
-	} else if (myNeck.y > myHead.y ||	(ruleset === "wrapped" && (myNeck.y === boardHeight - 1) && myHead.y === 0) ) {
+	}
+	if (isDeathUp(gameBoard, myHead, ruleset)) {
 		possibleMoves.up = -99999999999999;
 	}
-
-
-  // Avoid walls if game mode isn't wrapped
-	if (ruleset !== "wrapped") {
-		if (myHead.x === 0) {
-			possibleMoves.left = -99999999999999;
-		}
-		if (myHead.x === boardWidth - 1) {
-			possibleMoves.right = -99999999999999;
-		}
-		if (myHead.y === 0) {
-			possibleMoves.down = -99999999999999;
-		}
-		if (myHead.y === boardHeight - 1) {
-			possibleMoves.up = -99999999999999;
-		}
-	}
-
-	// Use information in gameState to prevent your Battlesnake from colliding with itself and others
-	const hazards: Coord[] = [];
-	gameState.board.snakes.forEach(snake => {
-		snake.body.forEach(bodyPiece => hazards.push(bodyPiece))
-	});
-
-	if (gameState.board.hazards) {
-		Array.prototype.push.apply(hazards, gameState.board.hazards);
-	}
-	const upDanger = hazards.some(hazard => {
-		return (hazard.x === myHead.x && hazard.y === myHead.y + 1)
-	})
-	if (upDanger) {
-		possibleMoves.up = -99999999999999;;
-	}
-	const downDanger = hazards.some(hazard => {
-		return (hazard.x === myHead.x && hazard.y === myHead.y - 1)
-	})
-	if (downDanger) {
-		possibleMoves.down = -99999999999999;;
-	}
-	const leftDanger = hazards.some(hazard => {
-		return (hazard.x === myHead.x - 1 && hazard.y === myHead.y)
-	})
-	if (leftDanger) {
-		possibleMoves.left = -99999999999999;;
-	}
-	const rightDanger = hazards.some(hazard => {
-		return (hazard.x === myHead.x + 1 && hazard.y === myHead.y)
-	})
-	if (rightDanger) {
-		possibleMoves.right = -99999999999999;;
+	if (isDeathDown(gameBoard, myHead, ruleset)) {
+		possibleMoves.down = -99999999999999;
 	}
 
 	// Find food
@@ -112,12 +164,12 @@ export function move(gameState: GameState): MoveResponse {
 	});
 	if (xDistance > 0) {
 		possibleMoves.right += Math.abs(boardWidth - Math.abs(xDistance));
-	} else {
+	} else if (xDistance < 0) {
 		possibleMoves.left += Math.abs(boardWidth - Math.abs(xDistance));
 	}
 	if (yDistance > 0) {
 		possibleMoves.up += Math.abs(boardHeight - Math.abs(yDistance));
-	} else {
+	} else if (yDistance < 0) {
 		possibleMoves.down += Math.abs(boardHeight - Math.abs(yDistance));
 	}
 
